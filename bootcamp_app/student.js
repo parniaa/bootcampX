aa1b2108488385a77762dd2a1d927d2b272a793c
paarmin <parniaa@gmail.com>
2021-02-04 00:49:35 -0500

    student and teacher js updated sql injection

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -4,16 +4,17 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-let cohName = process.argv[2];
-let listLimit = process.argv[3];
+const cohName = process.argv[2];
+const listLimit = process.argv[3];
+const values = [`%${cohName}%`, listLimit];
 pool.query(`
 SELECT students.id, students.name, cohorts.name as cname
 FROM students
 JOIN cohorts
 on cohort_id = cohorts.id
-where cohorts.name like '%${cohName}%'
-LIMIT ${listLimit};
-`)
+where cohorts.name like $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cname} cohort`);
